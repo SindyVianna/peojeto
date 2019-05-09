971a970914f4856322963237df583a47597a73ea
SindyVianna <49125198+SindyVianna@users.noreply.github.com>
2019-05-08 23:45:20 -0300

    Add files via upload

---
+++ projeto.js
@@ -0,0 +1,50 @@
+function goBack() {
+  window.history.back();
+}
+
+function Prioridade(){
+	var nome= document.getElementById("nome").value;
+	if(nome == ''){
+ alert(" O campo NOME eh obrigatorio! ");
+}
+else{
+alert("O Projeto foi cadastrado com sucesso:");
+}
+}
+
+
+
+
+
+function Nova()
+{
+location.href=" inicial.html ";
+}
+
+function Cadastrar()
+{
+location.href=" cadastro.html ";
+}
+
+function Consultar()
+{
+location.href=" consulta.html ";
+}
+
+function Alterar()
+{
+location.href=" alterar.html ";
+}
+
+function Excluir()
+{
+location.href=" excluir.html ";
+}
+ 
+
+
+
+
+ 
+
+
---
+++ salvar.php
@@ -0,0 +1,11 @@
+<?php
+     
+	 
+	 $vnome=$_GET["f_name"];
+     $vintro=$_GET["f_intro"];
+	 $varea=$_GET["f_area"];
+
+echo "Nome: ".$vnome."<br/>Introdução: ".$vintro."<br/> Area: ".$varea "<br/>";
+
+
+?>
